8527fd053dc5bb7b6e5dabd2d784e76b62f2649c
Long Long <longlong2010@gmail.com>
2017-06-23 16:47:26 +0800

    fix bugs

--- pointer/demo.py
+++ pointer/demo.py
@@ -2,15 +2,14 @@ import cv2;
 import numpy;
 import math;
 
-im = cv2.imread('test2.jpg');
+im = cv2.imread('1-resize.jpg');
 h = numpy.size(im, 0);
 w = numpy.size(im, 1);
 gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY);
 edges = cv2.Canny(gray, 10, 140, 3);
 cv2.imwrite('out.jpg', edges);
-lines = cv2.HoughLinesP(edges, 1, numpy.pi / 180, 100, 100, 50);
+lines = cv2.HoughLinesP(edges, 1, numpy.pi / 180, 100, 100, 30);
 circles = cv2.HoughCircles(edges, cv2.HOUGH_GRADIENT, 2, w / 4);
-
 circle_list = [];
 for x in circles[0]:
     if x[0] + x[2] < w and x[0] - x[2] > 0 and x[1] + x[2] < h and x[1] - x[2] > 0:
@@ -27,7 +26,7 @@ for x in lines:
     x1, y1, x2, y2 = x[0];
     v = numpy.array([c[0] - x1, c[1] - y1]);
     n = numpy.array([y1 - y2, x2 - x1]);
-    if abs(numpy.dot(v, n) / numpy.linalg.norm(v) < 10):
+    if abs(numpy.dot(v, n) / numpy.linalg.norm(n)) < 30:
         cv2.line(im, (x1, y1), (x2, y2),(0, 255, 0), 4);
         t = numpy.array([x2 - x1, y2 - y1]);
         print(math.acos(numpy.dot(t, numpy.array([1, 0]) / numpy.linalg.norm(t))) * 180 / numpy.pi);
