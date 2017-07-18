f3271eaf946277ee8c2a5bee6c8a3e524f2fd886
Long Long <longlong2010@gmail.com>
2017-07-18 15:15:45 +0000

    add result to video

--- pointer/camera.py
+++ pointer/camera.py
@@ -13,59 +13,60 @@ import pickle;
 import struct;
 import time;
 
-def process_image(im):
-    v0 = 10.0;
-    dv = 30.0 / 90.0;
-    h = numpy.size(im, 0);
-    w = numpy.size(im, 1);
-    #边缘处理
-    edges = cv2.Canny(im, 10, 140, 3);
-    #识别圆和直线
-    circles = cv2.HoughCircles(edges, cv2.HOUGH_GRADIENT if cv2.__version__ >= '3' else cv2.cv.CV_HOUGH_GRADIENT, 2, w / 4);
-    lines = cv2.HoughLinesP(edges, 1, numpy.pi / 180, 10, 100, 30);
-    if isinstance(circles, numpy.ndarray) == False or isinstance(lines, numpy.ndarray) == False:
-        return im;
-    #取整个都在图片内的最大的一个圆
-    circle_list = [];
-    for x in circles[0]:
-        if x[0] + x[2] < w and x[0] - x[2] > 0 and x[1] + x[2] < h and x[1] - x[2] > 0:
-            circle_list.append(x);
-    if len(circle_list) == 0:
-        return im;
-    c = circle_list[0];
-    for x in circle_list:
-        if x[2] > c[2]:
-            c = x;
-    cv2.circle(im, (c[0], c[1]), c[2], (255, 0, 0), 4);
-    
-    #取与找到的圆的距离小于阈值的直线
-    line_list = [];
-    for x in lines:
-        x1, y1, x2, y2 = x[0];
-        v = numpy.array([c[0] - x1, c[1] - y1]);
-        n = numpy.array([y1 - y2, x2 - x1]);
-        if abs(numpy.dot(v, n) / numpy.linalg.norm(n)) < 30:
-            cv2.line(im, (x1, y1), (x2, y2),(0, 255, 0), 4);
-            #指针确定方向
-            t1 = numpy.array([x1 - c[0], y1 - c[1]]);
-            t2 = numpy.array([x2 - c[0], y2 - c[1]]);
-            t = t1 - t2 if numpy.linalg.norm(t1) > numpy.linalg.norm(t2) else t2 - t1;
-            sgn = 1 if t[0] >= 0 else -1;
-            #计算指针和竖直方向的夹角
-            val = v0 + dv * math.acos(numpy.dot(t, numpy.array([0, -1]) / numpy.linalg.norm(t))) * 180 / numpy.pi * sgn;
-            t = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()));
-            print("%s\t%f" % (t, val));
-    return im;
-
 class ProcessThread(threading.Thread):
     def __init__(self, q):
         super(ProcessThread, self).__init__();
         self.q = q;
+        self.rs = "";
+    def process_image(self, im):
+        v0 = 10.0;
+        dv = 30.0 / 90.0;
+        h = numpy.size(im, 0);
+        w = numpy.size(im, 1);
+        #边缘处理
+        edges = cv2.Canny(im, 10, 140, 3);
+        #识别圆和直线
+        circles = cv2.HoughCircles(edges, cv2.HOUGH_GRADIENT if cv2.__version__ >= '3' else cv2.cv.CV_HOUGH_GRADIENT, 2, w / 4);
+        lines = cv2.HoughLinesP(edges, 1, numpy.pi / 180, 10, 100, 30);
+        if isinstance(circles, numpy.ndarray) == False or isinstance(lines, numpy.ndarray) == False:
+            return im;
+        #取整个都在图片内的最大的一个圆
+        circle_list = [];
+        for x in circles[0]:
+            if x[0] + x[2] < w and x[0] - x[2] > 0 and x[1] + x[2] < h and x[1] - x[2] > 0:
+                circle_list.append(x);
+        if len(circle_list) == 0:
+            return im;
+        c = circle_list[0];
+        for x in circle_list:
+            if x[2] > c[2]:
+                c = x;
+        cv2.circle(im, (c[0], c[1]), c[2], (255, 0, 0), 4);
+        
+        #取与找到的圆的距离小于阈值的直线
+        line_list = [];
+        for x in lines:
+            x1, y1, x2, y2 = x[0];
+            v = numpy.array([c[0] - x1, c[1] - y1]);
+            n = numpy.array([y1 - y2, x2 - x1]);
+            if abs(numpy.dot(v, n) / numpy.linalg.norm(n)) < 30:
+                cv2.line(im, (x1, y1), (x2, y2),(0, 255, 0), 4);
+                #指针确定方向
+                t1 = numpy.array([x1 - c[0], y1 - c[1]]);
+                t2 = numpy.array([x2 - c[0], y2 - c[1]]);
+                t = t1 - t2 if numpy.linalg.norm(t1) > numpy.linalg.norm(t2) else t2 - t1;
+                sgn = 1 if t[0] >= 0 else -1;
+                #计算指针和竖直方向的夹角
+                val = v0 + dv * math.acos(numpy.dot(t, numpy.array([0, -1]) / numpy.linalg.norm(t))) * 180 / numpy.pi * sgn;
+                t = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()));
+                self.rs = "%s  %f" % (t, val);
+                print(self.rs);
+        return im;
     def run(self):
         while True:
             if not self.q.empty():
                 im = self.q.get();
-                process_image(im);
+                self.process_image(im);
                 
 class StreamServer(threading.Thread):
     def __init__(self, q):
@@ -104,9 +105,12 @@ if __name__ == '__main__':
     
     while True:
         ret, im = cp.read();
-        #cv2.imshow('Video', im);
         if q1.empty():
             q1.put(im);
+
+        if worker.rs != "":
+            cv2.putText(im, worker.rs, (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, 2);
+        #cv2.imshow('Video', im);
         if q2.empty():
             q2.put(im);
         cv2.waitKey(int(1000 / fps));
